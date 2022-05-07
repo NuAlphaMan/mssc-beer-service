1e031baed82a33800369acfc45744dc0c1015799
NuAlphaMan <NuAlphaMan@users.noreply.github.com>
2022-05-07 19:27:32 -0400

    Initial commit of BeerController.

---
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,33 @@
+package guru.springframework.msscbeerservice.web.controller;
+
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("api/v1/beer")
+@RestController
+@Slf4j
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
+        //Todo: Impl - For getBeerById
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<HttpStatus> saveNewBeer(@RequestBody BeerDto beerDto) {
+        //Todo: Impl - For saveNewBeer
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity<BeerDto> updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+        //Todo: Impl - For updateBeerById
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
